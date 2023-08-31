ab3a389e1262a0b3656eeddf649d3b3ec4c0b806
MariLau-RE <71193755+MariLau-RE@users.noreply.github.com>
2023-08-30 22:26:09 -0600

    Incribirse a evento y evaluar evento

---
+++ app/src/main/java/com/app/heyaso/Inscribirse_a_evento.java
@@ -0,0 +1,29 @@
+package com.app.heyaso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Inscribirse_a_evento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscribirse_aevento);
+
+        Button button = (Button) findViewById(R.id.btn_volver);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                reOpenMenuPrincipalEstudiante();
+            }
+        });
+    }
+    public void reOpenMenuPrincipalEstudiante() {
+        Intent intent = new Intent(this, menuPrincipalEstudiante.class);
+        startActivity(intent);
+    }
+
+}
--- app/src/main/java/com/app/heyaso/Pantalla_Foro.java
+++ app/src/main/java/com/app/heyaso/Pantalla_Foro.java
@@ -1,6 +1,9 @@
 package com.app.heyaso;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,18 @@ public class Pantalla_Foro extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_foro);
+
+        Button button = (Button) findViewById(R.id.evaluarEvento);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                OpenEvaluar();
+            }
+        });
     }
+
+    public void OpenEvaluar() {
+        Intent intent = new Intent(this, Evaluar_Evento.class);
+        startActivity(intent);
+    }
+
 }
