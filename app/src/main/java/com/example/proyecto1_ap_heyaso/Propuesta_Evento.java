aaacecfec48d4fb9faa3cb744275241b2cefda2b
Mariana <mfernandezmar3@gmail.com>
2023-09-06 15:45:58 -0600

    pantalla propuesta evento

---
+++ app/src/main/java/com/example/proyecto1_ap_heyaso/Propuesta_Evento.java
@@ -0,0 +1,16 @@
+package com.example.proyecto1_ap_heyaso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.heyaso.R;
+
+public class Propuesta_Evento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_propuesta_evento);
+    }
+}
